51f4ad9ef7e6b721eec4804df2815317f15fd0af
Maciej Wygoda <maciej.wygoda@gmail.com>
2016-08-10 00:30:01 +0200

    2009/quali/A in progress 3

--- google code jam/2009/quali/A. Alien Language/main.cpp
+++ google code jam/2009/quali/A. Alien Language/main.cpp
@@ -7,14 +7,15 @@ using namespace std;
 int main()
 {
   //L - count of letters in a word, D - count of words in the dictionary, N - count of test cases
-  int L, D, N;
+  int L, D, N, indexToIncrease;
   string temp;
-  char token;
+  char character;
+  int possibleWords; //words possible to create from a pattern
   vector<string> dictionary;
-  vector<string> decipheredWords;
   vector<vector<char> > decipheredWord; //a word is a vector of vectors of chars (vector of chars represents a set of possible letters on one position in a word)
   vector<char> possibleLetters;
   cin >> L >> D >> N;
+  vector<int> characterIndicator;
 
 //getting dictionary
   for (size_t i = 0; i < D; i++)
@@ -27,22 +28,48 @@ int main()
   for (size_t i = 0; i < N; i++)
   {
     //getting a word to decipher
+    decipheredWord.clear();
     for (size_t j = 0; j < L; j++)
     {
-      decipheredWord.clear();
-      cin >> token;
-      if(token == '(')
+      possibleLetters.clear();
+      cin >> character;
+      if(character == '(')
       {
         do
         {
-          cin >> token;
-        } while(token != ')');
+          cin >> character;
+          possibleLetters.push_back(character);
+        } while(character != ')');
+      }
+      else
+      {
+        possibleLetters.push_back(character);
+      }
+      decipheredWord.push_back(possibleLetters);
+    }
+
+    //checking if any of the words from dictionary match the pattern
+    possibleWords = 1;
+    for (size_t i = 1; i <= L; i++)
+    {
+      possibleWords *= decipheredWord[i].size(); //e.g. possibleWords for (xyz)x(zrtz) would be 3*1*4 = 12
+    }
+    characterIndicator.clear();
+    for (size_t i = 0; i < characterIndicator.size(); i++)
+    {
+      characterIndicator.push_back(0);
+    }
+    indexToIncrease = L - 1;
+    for (size_t i = 0; i < possibleWords; i++)
+    {
+      if (characterIndicator[indexToIncrease] < decipheredWord[indexToIncrease].size() - 1)
+      {
+        characterIndicator[indexToIncrease]++;
       }
       else
       {
         
       }
-      
     }
   }
 
