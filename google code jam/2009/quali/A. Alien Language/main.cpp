b265a7e393e656833a5d84c30aa43dfba61b9cc2
Maciej Wygoda <maciej.wygoda@gmail.com>
2016-08-10 19:39:45 +0200

    2009/quali/A in progress 4

--- google code jam/2009/quali/A. Alien Language/main.cpp
+++ google code jam/2009/quali/A. Alien Language/main.cpp
@@ -1,77 +1,138 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-//I WANT TO DO THIS OBJECT-ORIENTED IN FUTURE
+/*
+NOTES:
+-I WANT TO DO THIS OBJECT-ORIENTED IN FUTURE
+-VARIABLES' NAMES COULD BE BETTER
+-SOME OF IT NEEDS OPTIMIZATION, BECAUSE IT CHECKS LITERALLY EACH CASE EVEN THOUGH SOME OF THEM SHOULD BE SKIPPED, APPROPRIATE INDICATOR IS IN THE CODE
+*/
+bool WordIsInDictionary(vector<string> dictionary, string word)
+{
+	for (size_t i = 0; i < dictionary.size(); i++)
+	{
+		if (dictionary[i] == word)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
 int main()
 {
-  //L - count of letters in a word, D - count of words in the dictionary, N - count of test cases
-  int L, D, N, indexToIncrease;
-  string temp;
-  char character;
-  int possibleWords; //words possible to create from a pattern
-  vector<string> dictionary;
-  vector<vector<char> > decipheredWord; //a word is a vector of vectors of chars (vector of chars represents a set of possible letters on one position in a word)
-  vector<char> possibleLetters;
-  cin >> L >> D >> N;
-  vector<int> characterIndicator;
+	//L - count of letters in a word, D - count of words in the dictionary, N - count of test cases
+	int L, D, N, indexToIncrease, iterator;
+	string temp;
+	string wordFromPattern = "";
+	char character;
+	long long possibleWordsCount; //words possible to create from a pattern
+  int matchingCount; //how many words from dictionary match the pattern
+	vector<string> dictionary;
+	vector<vector<char> > decipheredWord; //a word is a vector of vectors of chars (vector of chars represents a set of possible letters on one position in a word)
+	vector<char> possibleLetters;
+	cin >> L >> D >> N;
+	vector<int> characterIndicator; //e.g. 1,0,2 for (xyz)x(zrtz) means "yxt""
 
-//getting dictionary
-  for (size_t i = 0; i < D; i++)
-  {
-    cin >> temp;
-    dictionary.push_back(temp);
-  }
+									//getting dictionary
+	cin.ignore();
+	for (size_t i = 0; i < D; i++)
+	{
+		getline(cin, temp);
+		dictionary.push_back(temp);
+	}
 
-//main loop
-  for (size_t i = 0; i < N; i++)
-  {
-    //getting a word to decipher
-    decipheredWord.clear();
-    for (size_t j = 0; j < L; j++)
-    {
-      possibleLetters.clear();
-      cin >> character;
-      if(character == '(')
-      {
-        do
-        {
-          cin >> character;
-          possibleLetters.push_back(character);
-        } while(character != ')');
-      }
-      else
-      {
-        possibleLetters.push_back(character);
-      }
-      decipheredWord.push_back(possibleLetters);
-    }
+	//main loop
+	for (size_t i = 0; i < N; i++)
+	{
+		//getting a word to decipher
+		decipheredWord.clear();
+		iterator = 0;
+		getline(cin, temp);
 
-    //checking if any of the words from dictionary match the pattern
-    possibleWords = 1;
-    for (size_t i = 1; i <= L; i++)
-    {
-      possibleWords *= decipheredWord[i].size(); //e.g. possibleWords for (xyz)x(zrtz) would be 3*1*4 = 12
-    }
-    characterIndicator.clear();
-    for (size_t i = 0; i < characterIndicator.size(); i++)
-    {
-      characterIndicator.push_back(0);
-    }
-    indexToIncrease = L - 1;
-    for (size_t i = 0; i < possibleWords; i++)
-    {
-      if (characterIndicator[indexToIncrease] < decipheredWord[indexToIncrease].size() - 1)
-      {
-        characterIndicator[indexToIncrease]++;
-      }
-      else
-      {
-        
-      }
-    }
-  }
+		for (size_t j = 0; j < L; j++)
+		{
+			possibleLetters.clear();
+			character = temp[iterator++];
+			if (character == '(')
+			{
+				do
+				{
+					character = temp[iterator++];
+					possibleLetters.push_back(character);
+				} while (temp[iterator] != ')');
+				iterator++;
+			}
+			else
+			{
+				possibleLetters.push_back(character);
+			}
+			decipheredWord.push_back(possibleLetters);
+		}
 
-  return 0;
+		//checking if any of the words from dictionary match the pattern
+		possibleWordsCount = 1;
+		for (size_t j = 0; j < L; j++)
+		{
+			possibleWordsCount *= decipheredWord[j].size(); //e.g. possibleWordsCount for (xyz)x(zrtz) would be 3*1*4 = 12
+		}
+		characterIndicator.clear();
+		for (size_t j = 0; j < L; j++)
+		{
+			characterIndicator.push_back(0);
+		}
+		//firstly check the word constructed from first letters
+		wordFromPattern = "";
+		for (size_t j = 0; j < L; j++)
+		{
+			wordFromPattern += decipheredWord[j][0];
+		}
+		matchingCount = 0;
+		if (WordIsInDictionary(dictionary, wordFromPattern))
+		{
+			matchingCount++;
+		}
+		indexToIncrease = L - 1;
+		//this loop finds all possible words from a pattern and checks them against the dictionary
+    //OPTIMIZATION VERY MUCH NEEDED HERE, e.g. don't check 0xxxx for (mno)(zxcq)(zxcq)(zxcq)(zxcq) if there are no words starting with 'm' in the dictionary
+		//cout << endl;
+		for (size_t j = 0; j < possibleWordsCount - 1; j++)
+		{
+			//find the word
+			wordFromPattern = "";
+			while (characterIndicator[indexToIncrease] == decipheredWord[indexToIncrease].size() - 1)
+			{
+				indexToIncrease--;
+			}
+			for (size_t k = indexToIncrease + 1; k < L; k++)
+			{
+				characterIndicator[k] = 0;
+			}
+			characterIndicator[indexToIncrease]++;
+			indexToIncrease = L - 1;
+			for (size_t k = 0; k < L; k++)
+			{
+				wordFromPattern += decipheredWord[k][characterIndicator[k]];
+			}
+			//check the word
+			if (WordIsInDictionary(dictionary, wordFromPattern))
+			{
+				matchingCount++;
+			}
+      
+			// for (auto value : characterIndicator)
+			// {
+			// 	cout << value;
+			// }
+			// cout << endl;
+      
+		}
+		cout << "Case #" << i + 1 << ": " << matchingCount << endl;
+
+	}
+
+	return 0;
 }
